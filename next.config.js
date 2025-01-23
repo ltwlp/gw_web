5973e39303ef6b9eabc133f554f047dd47f02ffe
ltwlp <1319873777@qq.com>
2025-01-23 19:54:42 +0800

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true
   },
   images: {
+    unoptimized: true,
     domains: ['res.cloudinary.com']
-  }
+  },
+  basePath: '/gw_web',
 };
 
 module.exports = nextConfig;
